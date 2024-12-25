2b6010bf8547af2854751075e4e58e2c55331050
Jännät <191785648+Jaennaet@users.noreply.github.com>
2024-12-26 00:35:18 +0200

    comment

--- pISSStream/AppDelegate.swift
+++ pISSStream/AppDelegate.swift
@@ -10,6 +10,7 @@ import SwiftUI
 #if os(macOS)
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// accessory: the application doesn’t appear in the Dock and doesn’t have a menu bar, but it may be activated programmatically or by clicking on one of its windows.
         NSApp.setActivationPolicy(.accessory)
     }
 }
