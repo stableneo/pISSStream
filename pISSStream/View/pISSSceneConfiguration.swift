d4676fc5db46c34925d73832d60d1a205551dfa7
durul dalkanat <adurul17@hotmail.com>
2025-05-04 20:25:07 -0400

    Refactor pISSSceneConfiguration for improved volumetric window style and immersive experience

--- pISSStream/View/pISSSceneConfiguration.swift
+++ pISSStream/View/pISSSceneConfiguration.swift
@@ -9,23 +9,26 @@ import SwiftUI
 
 struct pISSSceneConfiguration: Scene {
     @StateObject private var appState = AppStateViewModel()
+    @State private var rotation:Double = 0
 
     var body: some Scene {
         #if os(visionOS)
             WindowGroup {
                 ContentView()
                     .environmentObject(appState)
+                    .background(Color.black).opacity(0.8)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
             }
 
-            .windowStyle(.automatic)
-            .defaultSize(width: 800, height: 500)
+            .windowStyle(.volumetric)
+            .defaultSize(width: 0.6, height: 0.6, depth: 0.4, in: .meters)
 
             ImmersiveSpace(id: "ISSSpace") {
                 pISSImmersiveView()
                     .environmentObject(appState)
             }
 
-            .immersionStyle(selection: .constant(.full), in: .full)
+            .immersionStyle(selection: .constant(.mixed), in: .mixed)
         #endif
     }
 }
